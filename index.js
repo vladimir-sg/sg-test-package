505c94c05fbd5966f1f1d1794ebb4cb9866b939d
Vladimir <c.vladimir@showcase-gig.com>
2022-06-02 15:45:33 +0900

    wip

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-'use strict'
+'use strict';
 
-module.exports = testFunc;
+// var strValue = String.prototype.valueOf;
+// var tryStringObject = function tryStringObject(value) {
+// 	try {
+// 		strValue.call(value);
+// 		return true;
+// 	} catch (e) {
+// 		return false;
+// 	}
+// };
+// var toStr = Object.prototype.toString;
+// var strClass = '[object String]';
+// var hasToStringTag = typeof Symbol === 'function' && !!Symbol.toStringTag;
 
-var testFunc = () => {
+module.exports = function isTest() {
+	// if (typeof value === 'string') {
+	// 	return true;
+	// }
+	// if (typeof value !== 'object') {
+	// 	return false;
+	// }
   console.log('testFunc: console.log');
   return 'return testFunc string';
-}
+	// return hasToStringTag ? tryStringObject(value) : toStr.call(value) === strClass;
+};
